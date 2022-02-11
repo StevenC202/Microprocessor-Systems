98ee87d2ef0e89be45b434c06649560ea17e00b0
StevenCataluna <94475285+StevenCataluna@users.noreply.github.com>
2022-02-11 16:34:44 +0000

    Add files via upload

--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -13,24 +13,33 @@
  * @return int      Returns exit-status zero on completion.
  */
 
-void float_wallis() {
-    float pi;
-    for(int i = 1; i < 100001; i++) {
-        pi = pi * ( (2*i/2*i-1) * (2*i/2*i+1) );
-    }
-    int approximation_error = ((pi - 3.14159265359) / 3.14159265359) * 100;
-    printf("The calculated value using floating point is %f\n", pi);
-    printf("The approximation error calculated for this value is %d", approximation_error);
-}
+/**
+    For loop to calculate wallis series needs to continuously multiply
+    the value for n -> infinity.
+*/
 
-void double_wallis() {
-    double pi;
-    for(int i = 1; i < 100001; i++) {
-        pi = pi * ( (2*i/2*i-1) * (2*i/2*i+1) );
+void float_wallis() {
+    float pi = 1.0; // starting value for wallis series
+    float actual_value = 3.14159265359;
+    for(float i = 1.0; i < 100000; i++) {
+        pi = pi * ( ((2.0*i)/(2.0*i-1.0) )* ( (2.0*i)/(2.0*i+1.0)) ); // wallis series calculated using a for loop 100,000 times
     }
-    int approximation_error = ((pi - 3.14159265359) / 3.14159265359) * 100;
+    pi = pi * 2; // double value calculated
+    float approximation_error = abs( ((pi - actual_value)/actual_value)*100.0); 
     printf("The calculated value using floating point is %f\n", pi);
-    printf("The approximation error calculated for this value is %d", approximation_error);
+    printf("The percentage error calculated for this value is %f%% \n ", approximation_error);
+}
+ 
+void double_wallis() {
+    double pi = 1.00;
+    double actual_value = 3.14159265359;
+    for(double i = 1.00; i < 100000; i++) {
+        pi = pi * ( ((2.00*i)/(2.00*i-1.00) )* ( (2.00*i)/(2.00*i+1.00)) ); // same for loop, except using double representation
+    }
+    pi = pi*2.0;
+    double approximation_error = abs(((pi - actual_value) / actual_value) * 100.00);
+    printf("The calculated value using double precision is %f\n", pi);
+    printf("The percentage error calculated for this value is %f%%\n", approximation_error);
 }
 
 
@@ -40,14 +49,14 @@ int main() {
 #ifndef WOKWI
     // Initialise the IO as we will be using the UART
     // Only required for hardware and not needed for Wokwi
-    stdio_init_all();
+    //stdio_init_all();
 #endif
 
     // Print a console message to inform user what's going on.
     printf("Calculating pi using floating point representation:\n");
     float_wallis();
 
-    printf("Calculating pi using double precision\n");
+    printf("Calculating pi using double precision:\n");
     double_wallis();
 
     // Returning zero indicates everything went okay.
